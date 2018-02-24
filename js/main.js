53a4d681089b965718bb2dbf703da50fa668a259
treyh23 <forrest@contempows.com>
2018-02-24 14:05:20 -0800

    first part of logic compeleted up to step 3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function main() {
-  $('.select-color').on('click'.function(),{
-    if(selectedColor === 'select-color cyan not-selected')  
+  $('.select-color').on('click',() => {
+    if(selectedColor === 'select-color cyan not-selected') {
+      return 
+    }  else if(selectedColor === '') {
+      
+    }
   }
 )
 };
