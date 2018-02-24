27a5d15a6664d046c1b08b27c34a0ffc04cd8c78
treyh23 <forrest@contempows.com>
2018-02-24 12:30:20 -0800

    new eslintrc

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function main() {
-  $('.select-color').on('click'.function()
-  
-);
-}
+  $('.select-color').on('click'.function(),{
+    if(selectedColor === 'select-color cyan not-selected')  
+  }
+)
+};
 
 $(document).ready(main);
