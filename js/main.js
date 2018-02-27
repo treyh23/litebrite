3c6c85e58a68bece3edf68aeac751eb3d5356c2b
treyh23 <forrest@contempows.com>
2018-02-26 22:21:59 -0800

    still working

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ let colorClass = '';
 
 function main() {
   $('.select-color').on('click',() => {
+    
     let selectedColor = $(this).attr('class')
+      
     if(selectedColor === 'select-color cyan not-selected') {
       colorClass = 'cyan';
-    } else if(selectedColor === 'select-color yellow not-selected') {
+      } else if(selectedColor === 'select-color yellow not-selected') {
       colorClass = 'yellow';
-    } else if('select-color magenta not-selected') {
+      } else if('select-color magenta not-selected') {
       colorClass = 'magenta';
-    }
+      }
     $(this).removeClass('not-selected');
     $(this).siblings().addClass('not-selected');
   }
